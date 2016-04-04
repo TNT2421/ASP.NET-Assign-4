f75f8e906176cb782efd2c534d109d7725c7c5be
TNT2421 <Djarvis-cc@conestogac.on.ca>
2016-04-04 10:50:12 -0400

    initial site

---
+++ ASP.NET MVC A4/ASP.NET MVC A4/Context/ApplicationDataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ASP.NET_MVC_A4.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ASP.NET_MVC_A4.Context
+{
+    public class ApplicationDataContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDataContext()
+            : base("DefaultConnection")
+        { }
+
+        public System.Data.Entity.DbSet<AppUser> AppUsers { get; set; }
+    }
+}
---
+++ ASP.NET MVC A4/ASP.NET MVC A4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.NET_MVC_A4.Startup))]
+namespace ASP.NET_MVC_A4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
