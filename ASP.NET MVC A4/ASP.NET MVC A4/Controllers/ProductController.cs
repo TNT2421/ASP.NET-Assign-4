731d3246dc605e32b3f18de2313851abf7116023
Lovelyaulakh <Hsingh8022@conestogac.on.ca>
2016-04-14 13:50:43 -0400

    Added basket but some errors

---
+++ ASP.NET MVC A4/ASP.NET MVC A4/Controllers/ProductController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_A4.Controllers
+{
+    public class ProductController : Controller
+    {
+    public class ProductsController : Controller
+    {
+        
+        IRepositoryBase<Product> products;
+        IRepositoryBase<Basket> baskets;
+        BasketService basketService;
+
+        public ProductsController(IRepositoryBase<Customer> customers, IRepositoryBase<Product> products, IRepositoryBase<Basket> baskets)
+        {
+            this.customers = customers;
+            this.products = products;
+            this.baskets = baskets;
+            basketService = new BasketService(this.baskets);
+        }
+
+
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            var model = products.GetAll();
+            return View(model);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var product = products.GetById(id);
+            return View(product);
+        }
+
+        public ActionResult AddToBasket(int id)
+        {
+            basketService.AddToBasket(this.HttpContext, id, 1);//always add one to the basket
+            return RedirectToAction("BasketSummary");
+        }
+        public ActionResult BasketSummary()
+        {
+            var model = basketService.GetBasket(this.HttpContext);
+            return View(model.BasketItems);
+        }
+
+
+    }
+}
