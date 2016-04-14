1264e3b85326ff19bd9a9b25d0a992b66828928c
Steven Bulgin <Sbulgin-cc@conestogac.on.ca>
2016-04-14 11:35:57 -0400

    Trying to sort it out

---
+++ ASP.NET MVC A4/ASP.NETMVCA4.Models/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP.NETMVCA4.Models
+{
+    public class Game
+    {
+        [Key]
+        public int Game_Id { get; set; }
+        public string GameName { get; set; }
+        public string GameStudio { get; set; }
+
+        public string ERSB { get; set; }
+
+        public string desc { get; set; }
+
+        public decimal price { get; set; }
+    }
+}
